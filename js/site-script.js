f6f6e6c98029bcf70b8e8d721d7042c63ccd769b
admindigpl <digpl@yandex.ru>
2016-12-08 21:40:20 +0800

    08-12-2016

---
+++ js/site-script.js
@@ -0,0 +1,9 @@
+ function showElem(id) {
+     var elem = document.getElementById(id);
+     elem.style.display = 'block';
+ }
+
+ function hiddenElem(id) {
+     var elem = document.getElementById(id);
+     elem.style.display = 'none';
+ }
